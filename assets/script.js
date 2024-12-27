97951278e6daa08bf1e52cf58fe998abf98a18f4
ryzntx <thalalatha13@gmail.com>
2024-12-28 03:50:43 +0700

    Add Morphtext functionality and update HTML structure

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    /* Rotating Text - Morphtext */
+    $("#js-rotating").Morphext({
+        animation: "fadeInUp",
+        separator: ",",
+        speed: 4000,
+    });
+});
