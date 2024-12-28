f503837bf8140fcd022de64aafb2a8bc3506c357
ryzntx <thalalatha13@gmail.com>
2024-12-28 10:15:21 +0700

    Add responsive navigation menu and update styles

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,12 @@ $(document).ready(function () {
         speed: 4000,
     });
 });
+
+function updatemenu() {
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  }else{
+    document.getElementById('menu').style.borderRadius = '10px';
+  }
+}
